90aa2f80ede61816b780b9aa59b538aabb32a92a
Kamil Pawlak <s34260@pjwstk.edu.pl>
2026-03-30 13:28:28 +0200

    zadanie11

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -216,7 +216,13 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie11_PolaczStudentowIZapisy()
     {
-        throw Niezaimplementowano(nameof(Zadanie11_PolaczStudentowIZapisy));
+        var joined = DaneUczelni.Studenci.Join(
+            DaneUczelni.Zapisy,
+            s => s.Id,
+            z => z.StudentId,
+            (s,z) => $"{s.Imie} {s.Nazwisko} {z.DataZapisu}"
+        );
+        return joined;
     }
 
     /// <summary>
